ba531ef5f55e79d91e1a76c5acb956722066eb55
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-08 11:13:01 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@
     			targets: '.ml3 .letter',
     			opacity: [0,1],
     			easing: "easeInOutQuad",
-    			duration: 2250 infinite,
+    			duration: infinite,
     			delay: (el, i) => 150 * (i+1)
   		}).add({
     			targets: '.ml3',
