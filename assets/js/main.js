f07dd95890382b1f544e39f1e7e8f7d66504fd59
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-08 12:01:30 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,18 +10,18 @@
 		$body = $('body');
 
 	// Wrap every letter in a span
-	var textWrapper = document.querySelector('.ml3');
+	var textWrapper = document.querySelector('.ml6 .letters');
 	textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 	anime.timeline({loop: true})
   		.add({
-    			targets: '.ml3 .letter',
-    			opacity: [0,1],
-    			easing: "easeInOutQuad",
-    			duration: 2250,
-    			delay: (el, i) => 150 * (i+1)
+    			targets: '.ml6 .letter',
+    			translateY: ["1.1em", 0],
+    			translateZ: 0,
+    			duration: 750,
+    			delay: (el, i) => 50 * i
   		}).add({
-    			targets: '.ml3',
+    			targets: '.ml6',
     			opacity: 0,
     			duration: 0,
     			easing: "",
