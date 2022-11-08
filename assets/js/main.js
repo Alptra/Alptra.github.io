fc16cdbeb90ac3fbe6a1ccc0794c5e8b3f5b4f0d
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-08 11:15:01 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,12 @@
 	var textWrapper = document.querySelector('.ml3');
 	textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-	anime.timeline({loop: false})
+	anime.timeline({loop: true})
   		.add({
     			targets: '.ml3 .letter',
     			opacity: [0,1],
     			easing: "easeInOutQuad",
-    			duration: infinite,
+    			duration: 100000,
     			delay: (el, i) => 150 * (i+1)
   		}).add({
     			targets: '.ml3',
