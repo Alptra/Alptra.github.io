b3b0a6b84631cae3502b7b051e426b8764a7bf89
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-08 18:54:22 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,21 @@
 
 (function($) {
 
+	$(window).scroll(function(){
+  var scrolledFromTop = $(window).scrollTop() + $(window).height();
+  $(".appear").each(function(){
+    var distanceFromTop = $(this).offset().top;
+    if(scrolledFromTop >= distanceFromTop+100){
+      console.log("hello");
+      var delaiAnim = $(this).data("delai");
+      $(this).delay(delaiAnim).animate({
+        top:0,
+        opacity:1
+      });
+    }
+  });
+});
+	
 	var	$window = $(window),
 		$body = $('body');
 
