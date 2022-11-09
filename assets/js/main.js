11e3eeb5c5877f622627ddbef68986c8f4b4c4d2
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-09 07:58:23 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 (function($) {
 	
 $(window).scroll(function(){
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 200);
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
   });
 
 /*win.scroll(function(){
