3f9c79e0b7adf1d37f308b1ae0d1b259d8f0c8cb
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-09 09:24:50 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,26 @@
 
 (function($) {
 	
+const callback = function (entries) {
+  entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate-fadeIn");
+    } else {
+      entry.target.classList.remove("animate-fadeIn");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".js-show-on-scroll");
+targets.forEach(function (target) {
+  target.classList.add("opacity-0");
+  observer.observe(target);
+});	
+	
 $(window).scroll(function(){
     $(".top").css("opacity", 1 - $(window).scrollTop() / 150);
   });
