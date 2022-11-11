959e0bb9fa829a26ae0b2f5b552b435d71656cff
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-11 10:11:02 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,32 +5,26 @@
 */
 
 (function($) {
-	
-	/* FadeIn Scroll */
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.fade').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},900);
-                    
-            }
-            
-        }); 
-    
-    });
-    
+	const callback = function (entries) {
+  entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate-fadeIn");
+    } else {
+      entry.target.classList.remove("animate-fadeIn");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".js-show-on-scroll");
+targets.forEach(function (target) {
+  target.classList.add("opacity-0");
+  observer.observe(target);
 });
-	
+
 	var	$window = $(window),
 		$body = $('body');
 
