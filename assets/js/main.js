cf190bb8c9af1e9c98c5ed8ea60e742f0a5b9bfb
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-11 15:13:09 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,6 @@
 */
 
 (function($) {
-	import { animate, scroll } from "motion";
-
-document.querySelectorAll("section > div").forEach((item) => {
-  scroll(animate(item, { opacity: [0, 1, 1, 0] }), {
-    target: item,
-    offset: ["start end", "end end", "start start", "end start"]
-  });
-});
 
 	var	$window = $(window),
 		$body = $('body');
