9e87b9336332e8be91717bf35be6ade26835cf9a
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-17 14:40:00 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,50 @@ targets.forEach(function (target) {
 				});
 
 })(jQuery);
+
+</script>
+// Fonction de défilement horizontal
+// el est l'élément HTML qui contiendra le contenu défilant
+// src est un tableau comprenant le chemin des images à faire défiler
+// pas est le pas d'incrémentation du défilement (défaut 1px)
+// tps est le temps entre deux incrémentations  (défaut 50ms)
+function defilImgHrz(el,srcs,pas,tps) {
+  if(typeof el=="string") { el = document.getElementById(el); }
+  var tps = tps || 50;
+  var pas = pas || 1;
+  var imgs = [];
+  var offset = 0;
+  for(var i=0,l=srcs.length;i<l;i++) {
+    var img = new Image();
+    img.src = srcs[i];
+    imgs.push(img);
+    img.style.height=el.offsetHeight+"px";
+    img.style.position = "absolute";
+    img.style.left = offset+"px";
+    el.appendChild(img);
+    offset += img.offsetWidth;
+  }
+  var first = 0;
+  var last = imgs.length-1;
+  (function d() {
+    for(var i=0,l=imgs.length;i<l;i++) {
+      var left = parseInt(imgs[i].style.left,10);
+      imgs[i].style.left = (left-pas)+"px";
+      if(i==first && (left-pas+imgs[i].offsetWidth)<0) {
+        imgs[i].style.left = 
+        (parseInt(imgs[last].style.left,10)+imgs[last].offsetWidth-(i==0?pas:0))+"px";
+	last = first++;
+        if(first>imgs.length-1) { first = 0; }
+      }
+    }
+    setTimeout(d,tps);
+  })();
+}
+// Appel des deux fonctions de défilement au chargement de ma page .
+  defilImgHrz('bandeau_horizontal',[
+    "photos/partenaire/1.jpg",
+    "photos/partenaire/1.jpg",
+    "photos/partenaire/1.jpg"
+  ]);
+};
+</script>
