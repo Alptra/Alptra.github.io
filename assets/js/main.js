c596d580f4ad9cac1004ca0389207aa3a71bc07a
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-20 21:25:48 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*
 	Alptra website
 
-	Template Strongly Typed by HTML5 UP, free for personal and commercial use under the CCA 3.0 license (html5up.net/license). Html5up.net | @ajlkn | aj@lkn.io | AJ.
+	html5up.net | Template Strongly Typed by HTML5 UP, free for personal and commercial use under the CCA 3.0 license (html5up.net/license). @ajlkn | aj@lkn.io | AJ.
 	Changes were made.
 */
 
