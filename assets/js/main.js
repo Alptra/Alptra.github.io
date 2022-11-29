e9636893f5a51f43fb546318eb683a94863ed215
Alptra <113446091+Alptra@users.noreply.github.com>
2022-11-29 16:14:40 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,48 @@ $(".nav li").mouseover(function () {
 	var	$window = $(window),
 		$body = $('body');
 
+// Button ID
+var buttonId = '#cookie-understand';
+// Cookie name
+var cookieName = 'lecookie';
+// Cookie value
+var cookieValue = 'le-cookie1';
+// Cookie expire (days)
+var cookieExpire = 10;
+
+// When click button, create cookie.
+$(document).ready(function(){
+  $(buttonId).click(function() {
+    if ($.cookie(cookieName) == null){
+
+      // Create the cookie.
+      $.cookie(cookieName, cookieValue, { expires: cookieExpire, path: '/' });
+
+      // Hide the alert message.
+      $('.cookie-bar').removeClass('cookie-bar--active');
+    }
+  });
+
+  checkCookie();
+
+  $('body').on('click', '#delete-cookie', function() {
+    
+    // Delete the cookie
+    $.removeCookie(cookieName, { path: '/' }); 
+  
+    checkCookie();        
+  });
+
+}); 
+
+// If no cookie, show the cookie bar.
+function checkCookie(){
+    if ($.cookie(cookieName) == null) {
+      // No cookie = Show cookie bar
+      $('.cookie-bar').addClass('cookie-bar--active');
+    }
+}		
+	
 	// Wrap every letter in a span
 	var textWrapper = document.querySelector('.ml6 .letters');
 	textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -251,47 +293,5 @@ $(document).ready(function($) {
     return false;
   });
 });
-	
-// Button ID
-var buttonId = '#cookie-understand';
-// Cookie name
-var cookieName = 'lecookie';
-// Cookie value
-var cookieValue = 'le-cookie1';
-// Cookie expire (days)
-var cookieExpire = 10;
-
-// When click button, create cookie.
-$(document).ready(function(){
-  $(buttonId).click(function() {
-    if ($.cookie(cookieName) == null){
-
-      // Create the cookie.
-      $.cookie(cookieName, cookieValue, { expires: cookieExpire, path: '/' });
-
-      // Hide the alert message.
-      $('.cookie-bar').removeClass('cookie-bar--active');
-    }
-  });
-
-  checkCookie();
-
-  $('body').on('click', '#delete-cookie', function() {
-    
-    // Delete the cookie
-    $.removeCookie(cookieName, { path: '/' }); 
-  
-    checkCookie();        
-  });
-
-}); 
-
-// If no cookie, show the cookie bar.
-function checkCookie(){
-    if ($.cookie(cookieName) == null) {
-      // No cookie = Show cookie bar
-      $('.cookie-bar').addClass('cookie-bar--active');
-    }
-}	
 	
 })(jQuery);
